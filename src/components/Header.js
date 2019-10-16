4198c9de0647e073c0aa7dbc562ca3ee089ed618
morwen80 <ribesnero80@gmail.com>
2019-10-16 16:24:07 +0100

    add transition to nav buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-class Header extends Component{
+class Header extends Component {
 
-  render(){
-    return(
-      <nav>
+render(){
+  return (
+    <nav>
       <ul>
         <NavLink id="navLogo" activeClassName="is-active" to='/'>Kronikles</NavLink>
-        <NavLink activeClassName="is-active" to='/'><i className="fas fa-copy" alt="All Kronikles"></i></NavLink>
-        <NavLink to='/new'><i className="fas fa-plus" alt="Add Kronikle"></i></NavLink>
+        <NavLink id="navShowAll" activeClassName="is-active" to='/'><i className="fas fa-copy" alt="All Kronikles"></i></NavLink>
+        <NavLink id="navAddNew" to='/new'><i className="fas fa-plus" alt="Add Kronikle"></i></NavLink>
 
         <NavLink to='/ADDLINK'><i className="fas fa-share-alt" alt="Share"></i></NavLink>
-        <NavLink to='/about'><i className="fas fa-question" alt="About"></i></NavLink>
-    </ul>
-
-      </nav>
+        <NavLink id="navAbout" to='/about'><i className="fas fa-question" alt="About"></i></NavLink>
+      </ul>
+    </nav>
     )
   }
 }
-
 
 export default Header
